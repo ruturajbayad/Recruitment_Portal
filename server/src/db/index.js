d3473a2e3cce096d78a36f8da52b30bbe4b16014
Ruturaj Bayad <ruturajsinhbayad@gmail.com>
2024-02-27 20:37:45 +0530

    First commit

---
+++ server/src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constents.js";
+
+const connectDb = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\nConnected to MongoDB 😎 ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection FAILED", error);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
---
+++ server/src/index.js
@@ -0,0 +1,21 @@
+import { app } from "./app.js";
+import dotenv from "dotenv";
+import connectDb from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
+
+// app.listen(process.env.PORT || 8000, () => {
+//   console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+// });
